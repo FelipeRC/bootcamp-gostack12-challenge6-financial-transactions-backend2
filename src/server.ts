38f719761b8456824c4806e4cc2e86d4bfade4da
Felipe <feliperosacruz@gmail.com>
2020-07-10 01:08:03 +0100

    Error treatment.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
-import { NextFunction, Request, Response } from 'express';
 import app from './app';
-import AppError from './errors/AppError';
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
